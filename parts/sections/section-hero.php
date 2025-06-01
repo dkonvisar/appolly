11c8b42a938552ed580ea99307b4aefa68222c6c
Denys Konvisar <denys.konvisar@yalantis.net>
2025-06-01 14:38:53 +0300

    added hero section

---
+++ parts/sections/section-hero.php
@@ -0,0 +1,29 @@
+<section class="hero">
+    <div class="container">
+        <div class="hero__inner">
+            <div class="hero__left">
+                <div class="hero__box-wrapper">
+                    <div class="hero__box">
+                        <h1>A Great App Makes Your Life Better</h1>
+                        <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+                        <div class="hero__download">
+                            <h3>Download App Now</h3>
+                            <div class="hero__download-links">
+                                <a href="#" target="_blank">
+                                    <img src="<?php echo THEME_DIR_URL . '/images/google-play.png' ?>" alt="Google play">
+                                </a>
+                                <a href="#" target="_blank">
+                                    <img src="<?php echo THEME_DIR_URL . '/images/app-store.png' ?>" alt="App store">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="hero__box-border"></div>
+                </div>
+            </div>
+            <div class="hero__right">
+                <img src="<?php echo THEME_DIR_URL . '/images/phone-image.png' ?>" alt="App Showcase">
+            </div>
+        </div>
+    </div>
+</section>
