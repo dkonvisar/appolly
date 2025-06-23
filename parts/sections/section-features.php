8b8db1cc1fbb38cf9e8c4c65ec916beb1506097b
Denys Konvisar <denys.konvisar@yalantis.net>
2025-06-23 10:19:44 +0300

    added features section

---
+++ parts/sections/section-features.php
@@ -0,0 +1,53 @@
+<section id="features" class="features">
+    <div class="container">
+        <div class="section-heading">
+            <h2>App Features</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra nunc ante velit vitae. Est tellus vitae, nullam lobortis enim. Faucibus amet etiam tincidunt rhoncus.</p>
+        </div>
+        <div class="features__items">
+            <div class="features__items-top">
+                <div class="features__item">
+                    <svg><use xlink:href="<?php echo THEME_DIR_URL . '/media/sprite.svg#icon-message'; ?>"></use></svg>
+                    <h3>Full free chat</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+            <div class="features__items-center">
+                <div class="features__items-left">
+                    <div class="features__item">
+                        <svg><use xlink:href="<?php echo THEME_DIR_URL . '/media/sprite.svg#icon-present'; ?>"></use></svg>
+                        <h3>unlimited features</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                    <div class="features__item">
+                        <svg><use xlink:href="<?php echo THEME_DIR_URL . '/media/sprite.svg#icon-ink'; ?>"></use></svg>
+                        <h3>awesome ui design</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+                <div class="features__image">
+                    <img src="<?php echo THEME_DIR_URL . '/media/income-screen.png' ?>" alt="Income app screen">
+                </div>
+                <div class="features__items-right">
+                    <div class="features__item">
+                        <svg><use xlink:href="<?php echo THEME_DIR_URL . '/media/sprite.svg#icon-smartphone'; ?>"></use></svg>
+                        <h3>iso & android version</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                    <div class="features__item">
+                        <svg><use xlink:href="<?php echo THEME_DIR_URL . '/media/sprite.svg#icon-eye-scanner'; ?>"></use></svg>
+                        <h3>retina ready graphics</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="features__items-bottom">
+                <div class="features__item">
+                    <svg><use xlink:href="<?php echo THEME_DIR_URL . '/media/sprite.svg#icon-support'; ?>"></use></svg>
+                    <h3>24/7 support by real people</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- templates/template-home.php
+++ templates/template-home.php
@@ -7,6 +7,7 @@ get_header();
 <main>
     <?php get_template_part('parts/sections/section', 'hero'); ?>
     <?php get_template_part('parts/sections/section', 'about'); ?>
+    <?php get_template_part('parts/sections/section', 'features'); ?>
     <?php // get_template_part('parts/sections/section', 'checkout'); ?>
     <?php // get_template_part('parts/sections/section', 'contacts'); ?>
 </main>
