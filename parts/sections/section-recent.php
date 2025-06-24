8eb027b0df8a578b0777d09763e559f30823043e
Denys Konvisar <denys.konvisar@yalantis.net>
2025-06-24 23:44:24 +0300

    added recent section

---
+++ parts/sections/section-recent.php
@@ -0,0 +1,45 @@
+<section id="recent" class="recent">
+    <div class="container">
+        <div class="section-heading">
+            <h2>Our recent blog</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra nunc ante velit vitae. Est tellus vitae,
+                nullam lobortis enim. Faucibus amet etiam tincidunt rhoncus, ullamcorper velit. Ullamcorper risus
+                tempor, ac nunc libero urna, feugiat.</p>
+        </div>
+        <div class="recent__cards">
+            <a class="recent__card" href="#">
+                <div class="recent__image">
+                    <img src="<?php echo THEME_DIR_URL . '/media/first-post.jpg' ?>" alt="Phone on yellow background">
+                </div>
+                <div class="recent__text">
+                    <h3>The Snap Pixel: How It Works and How to Install </h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra nunc ante velit vitae. Est
+                        tellus vitae, nullam lobortis enim.</p>
+                    <span>Read more</span>
+                </div>
+            </a>
+            <a class="recent__card" href="#">
+                <div class="recent__image">
+                    <img src="<?php echo THEME_DIR_URL . '/media/second-post.jpg' ?>" alt="Phone on the table">
+                </div>
+                <div class="recent__text">
+                    <h3>The Snap Pixel: How It Works and How to Install </h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra nunc ante velit vitae. Est
+                        tellus vitae, nullam lobortis enim.</p>
+                    <span>Read more</span>
+                </div>
+            </a>
+            <a class="recent__card" href="#">
+                <div class="recent__image">
+                    <img src="<?php echo THEME_DIR_URL . '/media/third-post.jpg' ?>" alt="Phone in the hand">
+                </div>
+                <div class="recent__text">
+                    <h3>The Snap Pixel: How It Works and How to Install </h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra nunc ante velit vitae. Est
+                        tellus vitae, nullam lobortis enim.</p>
+                    <span>Read more</span>
+                </div>
+            </a>
+        </div>
+    </div>
+</section>
