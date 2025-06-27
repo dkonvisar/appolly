caece1af9a88c3267b8795229801226541deb2f3
Denys Konvisar <denys.konvisar@yalantis.net>
2025-06-27 21:28:53 +0300

    changed checkout slider

--- assets/src/js/modules/checkout-slider.js
+++ assets/src/js/modules/checkout-slider.js
@@ -4,20 +4,47 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 
 const checkoutSlider = () => {
-  new Swiper('.checkout__slider', {
+  const updateSlideClasses = (swiper) => {
+    const slides = swiper.slides;
+
+    // Clean up previous custom classes
+    slides.forEach((slide) => {
+      slide.classList.remove('slide-before-prev', 'slide-after-next');
+    });
+
+    const activeSlide = swiper.slides[swiper.activeIndex];
+    if (!activeSlide) return;
+
+    // Get 2 previous slides in DOM
+    let prev = activeSlide.previousElementSibling;
+    if (prev?.previousElementSibling) {
+      prev.previousElementSibling.classList.add('slide-before-prev');
+    }
+
+    // Get 2 next slides in DOM
+    let next = activeSlide.nextElementSibling;
+    if (next?.nextElementSibling) {
+      next.nextElementSibling.classList.add('slide-after-next');
+    }
+  };
+
+  const swiper = new Swiper('.checkout__slider', {
     modules: [Pagination, Navigation],
     slidesPerView: 'auto',
     centeredSlides: true,
-    spaceBetween: 20,
-    initialSlide: 1,
+    grabCursor: true,
+    spaceBetween: 0,
     loop: true,
     pagination: {
       el: '.swiper-pagination-custom',
       clickable: true,
     },
-    breakpoints: {
-      768: {
-        slidesPerView: 3,
+    on: {
+      init() {
+        updateSlideClasses(this);
+      },
+      slideChangeTransitionStart() {
+        updateSlideClasses(this);
       },
     },
   });
--- parts/sections/section-checkout.php
+++ parts/sections/section-checkout.php
@@ -4,22 +4,39 @@
             <h2>Checkout Our App Interface Look</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra nunc ante velit vitae. Est tellus vitae, nullam lobortis enim. Faucibus amet etiam tincidunt rhoncus, ullamcorper velit. Ullamcorper risus tempor, ac nunc libero urna, feugiat.</p>
         </div>
+    </div>
+    <div class="checkout__slider-container container">
         <div class="checkout__slider swiper">
             <div class="swiper-wrapper">
                 <div class="checkout__slide swiper-slide">
-                    <img src="<?php echo THEME_DIR_URL . '/media/checkout-3.png' ?>" alt="Mobile">
+                    <div class="checkout__image">
+                        <img src="<?php echo THEME_DIR_URL . '/media/checkout-4.png' ?>" alt="Mobile screen">
+                    </div>
                 </div>
                 <div class="checkout__slide swiper-slide">
-                    <img src="<?php echo THEME_DIR_URL . '/media/checkout-3.png' ?>" alt="Mobile">
+                    <div class="checkout__image">
+                        <img src="<?php echo THEME_DIR_URL . '/media/checkout-2.png' ?>" alt="Mobile screen">
+                    </div>
                 </div>
                 <div class="checkout__slide swiper-slide">
-                    <img src="<?php echo THEME_DIR_URL . '/media/checkout-3.png' ?>" alt="Mobile">
+                    <div class="checkout__image">
+                        <img src="<?php echo THEME_DIR_URL . '/media/checkout-4.png' ?>" alt="Mobile screen">
+                    </div>
                 </div>
                 <div class="checkout__slide swiper-slide">
-                    <img src="<?php echo THEME_DIR_URL . '/media/checkout-3.png' ?>" alt="Mobile">
+                    <div class="checkout__image">
+                        <img src="<?php echo THEME_DIR_URL . '/media/checkout-3.png' ?>" alt="Mobile screen">
+                    </div>
                 </div>
                 <div class="checkout__slide swiper-slide">
-                    <img src="<?php echo THEME_DIR_URL . '/media/checkout-3.png' ?>" alt="Mobile">
+                    <div class="checkout__image">
+                        <img src="<?php echo THEME_DIR_URL . '/media/checkout-3.png' ?>" alt="Mobile screen">
+                    </div>
+                </div>
+                <div class="checkout__slide swiper-slide">
+                    <div class="checkout__image">
+                        <img src="<?php echo THEME_DIR_URL . '/media/checkout-1.png' ?>" alt="Mobile screen">
+                    </div>
                 </div>
             </div>
             <div class="swiper-pagination-custom"></div>
